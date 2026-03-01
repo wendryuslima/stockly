e83d86f58bc014c7fe264e7083d70aea8ea1bb16
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2026-03-01 13:48:45 -0300

    feat: add imagem in title aplication

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,12 @@ import "./globals.css";
 import SidebarLayout from "./_components/sidebar-layout";
 import { Toaster } from "@/components/ui/sonner";
 
-
 export const metadata: Metadata = {
   title: "Stockly",
   description: "Stockly é um sistema de gestão de estoque",
+  icons: {
+    icon: "/title-stockly.png",
+  },
 };
 
 const inter = Inter({
@@ -23,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <SidebarLayout>{children}</SidebarLayout>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
